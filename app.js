645d897c88e180fefd5624213c10f0ae630bfceb
Peter Yu <PeterYu@gmail.com>
2017-07-30 15:37:05 -0400

    migrated code from index.html to app.js

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-//   function initMap() {
-//     var map = new google.maps.Map(document.getElementById('map'), {
-//       zoom: 14,
-//       center: {lat: 33, lng: -83}
-//     });
+    function initMap() {
+        
+    var map = new google.maps.Map(document.getElementById('map'),  {  
+        center: {lat: 34, lng: -83},  
+        mapTypeId: "terrain",  
+        zoom: 11  
+        });
 
-//     var bikeLayer = new google.maps.BicyclingLayer();
-//     bikeLayer.setMap(map);
-//   }
+    var bikeLayer = new google.maps.BicyclingLayer();
+    bikeLayer.setMap(map);
 
-//   function watchSubmit() {
-//     $('.js-search-form').submit(event => {
-//       event.preventDefault();
-//       const queryTarget = $(event.currentTarget).find('.js-query');
-//       const query = queryTarget.val();
-//       queryTarget.val("");
-//       getDataFromApi(query);
-//     });
-//   }
+    var t = new Date().getTime();  
+    var waqiMapOverlay = new google.maps.ImageMapType( {  
+        getTileUrl: function(coord, zoom) {  
+        return 'https://tiles.waqi.info/tiles/usepa-aqi/' + zoom + "/" + coord.x + "/" + coord.y + ".png?token=7af2191ef71f827a4de1cbcdd9463989a7c3bb6c";},  
+        name: "Air Quality",  
+    });  
+
+    map.overlayMapTypes.insertAt(0,waqiMapOverlay);
+    }
+initMap();
