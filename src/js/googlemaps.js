2270fe4f1ac62247f698f66aa60f8d919a316091
Peter Yu <PeterYu@gmail.com>
2017-09-01 00:02:23 -0400

    added javascript messages for the components

--- src/js/googlemaps.js
+++ src/js/googlemaps.js
@@ -1,8 +1,8 @@
 function initAutocomplete() {
   $(function(){
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37, lng: -95},
-    zoom: 5,
+    center: {lat: 47.6147, lng: -122},
+    zoom: 13,
     disableDefaultUI: true,
     mapTypeId: 'terrain',
   });
@@ -10,6 +10,9 @@ function initAutocomplete() {
   // Create the search box and link it to the UI element.
   var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
+
+  var bikeLayer = new google.maps.BicyclingLayer();
+  bikeLayer.setMap(map);
 
   // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
@@ -27,9 +30,6 @@ function initAutocomplete() {
   };
 
   searchBox.addListener('places_changed', function() {
-
-    var bikeLayer = new google.maps.BicyclingLayer();
-    bikeLayer.setMap(map);
 
     var places = searchBox.getPlaces();
 
@@ -74,7 +74,7 @@ function initAutocomplete() {
       }
     });
     map.fitBounds(bounds);
-    map.setZoom(15);
+    map.setZoom(14);
     $('html, body').animate({ scrollTop: $('main').offset().top - 20});
   
     const latVal = searchBox.getPlaces()[0].geometry.location.lat();
@@ -100,6 +100,16 @@ function initAutocomplete() {
       });
 
         $.when(weatherCall, aqiCall).done(function (firstResponse, secondResponse) {
+          
+          const rain = Math.round(firstResponse[0].currently.precipProbability*100);
+          const humid = Math.round(firstResponse[0].currently.humidity*100);
+          const temp = Math.round(Number(firstResponse[0].currently.temperature));
+          const wind = Math.round(firstResponse[0].currently.windSpeed);
+          const wind_gust = firstResponse["0"].currently.windGust;
+          const wind_bearing = firstResponse[0].currently.windBearing;
+          const uv = Math.round(firstResponse[0].currently.uvIndex);
+          const aqi = Math.round(secondResponse[0].data.aqi);
+          const aqi_updated_time = secondResponse["0"].data.time.s;
 
           function aqiMessage() {if (secondResponse[0].data.aqi <= 50) {
               return "Air quality is considered satisfactory, and air pollution poses little or no risk."
@@ -127,55 +137,102 @@ function initAutocomplete() {
               return "Extreme risk of harm from unprotected sun exposure. Take all precautions because unprotected skin and eyes can burn in minutes. "}
           };
 
+          function windMessage() {if (firstResponse[0].currently.windSpeed < 1) {
+            return "Conditions are best described as calm. Smoke rises vertically."
+          } else if (firstResponse[0].currently.windSpeed <= 3) {
+            return "Conditions are best described as light. Wind motion visible in smoke."
+          } else if (firstResponse[0].currently.windSpeed <= 7) {
+            return "Wind is best described as a light breeze. Wind felt on exposed skin. Leaves rustle."
+          } else if (firstResponse[0].currently.windSpeed <= 12) {
+            return "Wind is best described as a gentle breeze. Leaves and smaller twigs in constant motion."
+          } else if (firstResponse[0].currently.windSpeed <= 18) {
+            return "Wind is best described as a moderate breeze. Dust and loose paper is raised. Small branches begin to move."
+          } else if (firstResponse[0].currently.windSpeed <= 24) {
+            return "Wind is best described as a fresh breeze. Smaller trees sway."
+          } else if (firstResponse[0].currently.windSpeed <= 31) {
+            return "Wind is best described as a strong breeze. Large branches in motion in motion. Whistling heard in overhead wires. Umbrella use becomes difficult."
+          } else if (firstResponse[0].currently.windSpeed <= 38) {
+            return "Wind is nearly a gale. Whole trees in motion. Some difficulty when walking into the wind."
+          } else if (firstResponse[0].currently.windSpeed <= 46) {
+            return "Wind is best described as a gale. Twigs broken from trees. Cars veer on road."
+          } else if (firstResponse[0].currently.windSpeed <= 54) {
+            return "Wind is best described as a severe gale. Expect light structure damage."
+          } else if (firstResponse[0].currently.windSpeed <= 63) {
+            return "Conditions are best described as a storm. Expect trees uprooted and considerable structural damage."
+          } else if (firstResponse[0].currently.windSpeed <= 73) {
+            return "Conditions are best described as a violent storm. Expect widespread structural damage."
+          } else if (firstResponse[0].currently.windSpeed <= 95) {
+            return "A hurricane is coming.  Expect considerable and widespread damage to structures."
+          }
+        };
+
           function windDirection() {if (firstResponse[0].currently.windBearing <= 11.25) {
               return "N"
-            } else if (firstResponse[0].currently.windBearing <= 33.75) {
+            } else if (wind_bearing <= 33.75) {
               return "NNE"
-            } else if (firstResponse[0].currently.windBearing <= 56.25) {
+            } else if (wind_bearing <= 56.25) {
               return "NE"
-            } else if (firstResponse[0].currently.windBearing <= 78.75) {
+            } else if (wind_bearing <= 78.75) {
               return "ENE"
-            } else if (firstResponse[0].currently.windBearing <= 101.25) {
+            } else if (wind_bearing <= 101.25) {
               return "E"
-            } else if (firstResponse[0].currently.windBearing <= 123.75) {
+            } else if (wind_bearing <= 123.75) {
               return "ESE"
-            } else if (firstResponse[0].currently.windBearing <= 146.25) {
+            } else if (wind_bearing <= 146.25) {
               return "SE"
-            } else if (firstResponse[0].currently.windBearing <= 168.75) {
+            } else if (wind_bearing <= 168.75) {
               return "SSE"
-            } else if (firstResponse[0].currently.windBearing <= 191.25) {
+            } else if (wind_bearing <= 191.25) {
               return "S"
-            } else if (firstResponse[0].currently.windBearing <= 213.75) {
+            } else if (wind_bearing <= 213.75) {
               return "SSW"
-            } else if (firstResponse[0].currently.windBearing <= 236.25) {
+            } else if (wind_bearing <= 236.25) {
               return "SW"
-            } else if (firstResponse[0].currently.windBearing <= 258.75) {
+            } else if (wind_bearing <= 258.75) {
               return "WSW"
-            } else if (firstResponse[0].currently.windBearing <= 281.25) {
+            } else if (wind_bearing <= 281.25) {
               return "W"
-            } else if (firstResponse[0].currently.windBearing <= 303.75) {
+            } else if (wind_bearing <= 303.75) {
               return "WNW"
-            } else if (firstResponse[0].currently.windBearing <= 326.25) {
+            } else if (wind_bearing <= 326.25) {
               return "NW"
-            } else if (firstResponse[0].currently.windBearing <= 348.75) {
+            } else if (wind_bearing <= 348.75) {
               return "NNW"
             } else {
               return "N"}
           };
 
-          const rain = Math.round(firstResponse[0].currently.precipProbability*100);
-          const humid = Math.round(firstResponse[0].currently.humidity*100);
-          const temp = Math.round(Number(firstResponse[0].currently.temperature));
-          const wind = Math.round(firstResponse[0].currently.windSpeed);
-          const uv = Math.round(firstResponse[0].currently.uvIndex);
-          const aqi = Math.round(secondResponse[0].data.aqi);
-
+        function humidityMessage() {if (firstResponse[0].currently.humidity <= .20) {
+            return "Outdoor relative humidity levels are extremely low. Expect dry air."
+          } else if (firstResponse[0].currently.humidity <= .45) {
+            return "Outdoor relative humidity levels are acceptable."
+          } else if (firstResponse[0].currently.humidity <= .60) {
+            return "Outdoor relative humidity levels are optimal."
+          } else if (firstResponse[0].currently.humidity <= .70) {
+            return "Outdoor relative humidity levels are acceptable."
+          } else {
+            return "Outdoor relative humidity levels are high. Expect a lot of moisture in the air."
+          }
+        };
+ 
           $("#t-temp-value").html(`${temp}°F`);
-          $("#t-humid-value").html(`${humid}%`);
-          $("#t-wind-value").html(`${wind} mph ${windDirection()}`);
+          $("#t-humidity-value").html(`${humid}%`);
+          $("#t-wind-value").html(`${wind} mph`);
           $("#t-rain-value").html(`${rain}%`);
           $("#t-uv-value").html(`${uv}`);
           $("#t-aqi-value").html(`${aqi}`);
+          $("#t-temp-explanation").html(``);
+          $("#t-humid-explanation").html(`${humidityMessage()}`);
+          $("#t-wind-explanation").html(
+            `
+            <p>${windMessage()}</p>
+            <br />
+            <p>Coming from the direction of ${windDirection()}.</p>
+            `
+          );
+          $("#t-rain-explanation").html(``);
+          $("#t-uv-explanation").html(`${uviMessage()}`);
+          $("#t-aqi-explanation").html(`<p>${aqiMessage()}</p>`);
         })
       })
   })
