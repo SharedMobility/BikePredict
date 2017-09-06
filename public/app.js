34636a2d6343b425901ebf4c6e738c24cb9d1e4c
Peter Yu <PeterYu@gmail.com>
2017-09-05 20:40:07 -0400

    added functional modal

--- public/app.js
+++ public/app.js
@@ -7,11 +7,21 @@
     },
 
     toggleModal: function() {
-      $("#t-modal").on("click", function() {
-        alert("Hello World");
-      })
-    }
+      let modal = document.getElementById('myModal');
+      let span = document.getElementsByClassName("close")[0];
 
+      $("#t-modal").on("click", function() {
+        modal.style.display = "block";
+      })
+      span.onclick = function() {
+        modal.style.display = "none";
+      }
+      window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+      }
+    }
   };
 
   $(window).on('load', () => {
