cf302e0465c9504b4d0759c8bd8ceea8c3acfdd7
Peter Yu <PeterYu@gmail.com>
2018-03-31 12:28:44 -0400

    in the process of hiding clientside api tokens

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
 exports.PORT = process.env.PORT || 3002;
+exports.DARK_SKY_TOKEN = process.env.DARK_SKY_TOKEN;
+exports.AQI_TOKEN = process.env.AQI_TOKEN;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const {PORT} = require('./config');
 const app = express();
@@ -6,9 +7,10 @@ app.use( '/', express.static(__dirname + '/public') );
 app.use( '/node_modules', express.static(__dirname + '/node_modules') );
 app.use( '/src', express.static(__dirname + '/src') );
 
-app.get('/heartbeat', function(req, res) {
+app.get('/tokens', function(req, res) {
   res.json({
-    is: 'working'
+    dark_sky_token: process.env.DARK_SKY_TOKEN,
+    aqi_token: process.env.AQI_TOKEN
   })
 });
 
